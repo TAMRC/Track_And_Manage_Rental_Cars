62a928fe6033af60e6d99bef99db53e5ca606694
TAMRC <t.maisondieu@groupe-ncr.com>
2024-11-02 19:37:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  # Active CORS pour toutes les routes
 
 UPLOAD_FOLDER = '/tmp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -19,10 +21,6 @@ def upload_pdf():
     message = "Fichier PDF traité avec succès."
 
     return jsonify({'message': message})
-
-@app.route('/health')
-def health_check():
-    return "OK", 200
 
 if __name__ == "__main__":
     app.run(debug=True)
